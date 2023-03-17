e6eaf8024d9b67b66d53aa3aaeefb6e599db1ef3
Promlert Lovichit <promlert@gmail.com>
2023-03-17 23:49:31 +0700

    Add sample code for fetching students data from API

--- lib/pages/home/attendance_page.dart
+++ lib/pages/home/attendance_page.dart
@@ -1,3 +1,4 @@
+import 'package:classroom_attendance/models/student.dart';
 import 'package:classroom_attendance/services/api.dart';
 import 'package:flutter/material.dart';
 
@@ -9,23 +10,76 @@ class AttendancePage extends StatefulWidget {
 }
 
 class _AttendancePageState extends State<AttendancePage> {
+  List<Student>? _studentList;
+  bool _isLoading = false;
+  bool _isError = false;
+  String _errMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    setState(() {
+      _isLoading = true;
+      _isError = false;
+    });
+
+    await Future.delayed(const Duration(seconds: 3), () {});
+
+    try {
+      var result = await ApiClient().getAllStudents();
+      setState(() {
+        _studentList = result;
+      });
+    } catch (e) {
+      setState(() {
+        _errMessage = e.toString();
+        _isError = true;
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () async {
-          try {
-            var checkInSuccess = await ApiClient().studentCheckIn('555', 1);
-            debugPrint(checkInSuccess ? 'Check-in สำเร็จ' : 'เกิดข้อผิดพลาดในการ check-in');
-          } catch (e) {
-            debugPrint(e.toString());
-          }
-        },
-        child: const Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Text('Check In Class 4'),
-        ),
-      ),
+    return Stack(
+      children: [
+// error message และปุ่ม retry
+        if (_isError)
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(_errMessage),
+                ElevatedButton(
+                  onPressed: () {
+                    loadData();
+                  },
+                  child: const Text('RETRY'),
+                )
+              ],
+            ),
+          ),
+
+// listview
+        _studentList == null
+            ? const SizedBox.shrink()
+            : ListView.builder(
+                itemCount: _studentList!.length,
+                itemBuilder: (context, index) {
+                  return Text(_studentList![index].firstName);
+                },
+              ),
+
+// progress indicator
+        if (_isLoading) const Center(child: CircularProgressIndicator())
+      ],
     );
   }
 }
